d06efb8bc78145cd1014b7670f556f2f761cc58f
Muhammad <33785330+lpln25@users.noreply.github.com>
2023-04-29 04:29:29 +0330

    init

---
+++ Contexts/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppSaba.Models.Entities;
+
+namespace WebAppSaba.Contexts
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options):base(options) { }
+
+        public DbSet<User> Users { get; set; } 
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+        public DbSet<ChatHistory> ChatHistories { get; set; }
+        public DbSet<ChatFile> ChatFiles { get; set; }
+    }
+}
